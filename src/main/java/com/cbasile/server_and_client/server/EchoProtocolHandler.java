3d79d80046452b06e1d622a5eb2056e28354b7fa
c_basile <basile.christian94@gmail.com>
2023-03-03 11:38:12 -0500

    update ProtocolHandler to allow cross-connection communication

--- src/main/java/com/cbasile/server_and_client/server/EchoProtocolHandler.java
+++ src/main/java/com/cbasile/server_and_client/server/EchoProtocolHandler.java
@@ -1,9 +1,11 @@
 package com.cbasile.server_and_client.server;
 
+import java.util.function.BiConsumer;
+
 public class EchoProtocolHandler implements ProtocolHandler {
 
 	@Override
-	public String handleMsg(String id, String msg) {
+	public String handleMsg(String id, String msg, BiConsumer<String,String> friendlyMsgHandler) {
 		return String.format("echo '%s'", msg);
 	}
 
